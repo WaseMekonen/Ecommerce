8b465e115ad05852bf394008360d40a6e39f02a5
Wase Mekonen <wase@Wases-MacBook-Air.local>
2022-09-19 20:25:09 +0300

    ecommerce new version-fix main page responsive2

--- client/js/produtsPageFunctions.js
+++ client/js/produtsPageFunctions.js
@@ -8,17 +8,23 @@ function showItemByCategory(div, category, products) {
   for (let i = 0; i < products.length; i++) {
     if (products[i].category == category) {
       div.innerHTML += `<div class="card" id="card">
-      <div class="image" style="background-image: url(${products[i].image[0]})" onmouseover="this.style.backgroundImage='url(${products[i].image[1]})'" onmouseout="this.style.backgroundImage='url(${products[i].image[0]})'"></div>
+      <div class="image" style="background-image: url(${products[i].image[0]})"
+          onmouseover="this.style.backgroundImage='url(${products[i].image[1]})'"
+          onmouseout="this.style.backgroundImage='url(${products[i].image[0]})'"></div>
       <div class="details">
-        <h4>${products[i].name}</h4>
-        <p>${products[i].description}</p>
-        <div class="card-bottom">
-          <button class="addtocart-button" onclick="addProductToCart('${products[i]._id}','${products[i].name}','${products[i].price}','${products[i].description}','${products[i].category}','${products[i].image[0]}','${products[i].quantity}')" >add to cart</button>
-          <button class="delete-button" id="delete-button" onclick="deleteItem('${products[i]._id}')" >delete</button>
-          <span><h5>${products[i].price}$</h5></span>
-        </div>
+          <h4>${products[i].name}</h4>
+          <p>${products[i].description}</p>
       </div>
-      </div>`;
+      <div class="card-bottom">
+          <button class="addtocart-button"
+              onclick="addProductToCart('${products[i]._id}','${products[i].name}','${products[i].price}','${products[i].description}','${products[i].category}','${products[i].image[0]}','${products[i].quantity}')">add
+              to cart</button>
+          <button class="delete-button" id="delete-button" onclick="deleteItem('${products[i]._id}')">delete</button>
+          <span>
+              <h5>${products[i].price}$</h5>
+          </span>
+      </div>
+  </div>`;
     }
   }
 }
@@ -27,51 +33,51 @@ function showItemByCategory(div, category, products) {
 axios.get("/products").then((response) => {
   showItemByCategory(cardContainer, pagesCategory.id, response.data)
 })
-.catch(
-  (err) => {
-    console.log(err);
-  }
-)
+  .catch(
+    (err) => {
+      console.log(err);
+    }
+  )
 
 
-function deleteItem (id){
+function deleteItem(id) {
   axios.delete(`/products/${id}`)
-  .then(response =>{
-    console.log(response);
-    alert("Item Has Been Deleted");
-  })
-  .catch(err=>{
-    console.log(err);
-  })
-}
-
-function addProductToCart (_id,name,price,description,category,image,quantity){
-  axios.patch("/carts/add",{
-    _id,name,price,description,category,image,quantity
-  })
-    .then(response =>{
+    .then(response => {
       console.log(response);
-      updateCartCounter()
+      alert("Item Has Been Deleted");
     })
-    .catch(err=>{
+    .catch(err => {
       console.log(err);
     })
 }
 
-function updateCartCounter(){
-  axios.get("/carts/619028c92121e8d9e3a2c533")
-  .then((res) => {
-    const counter = res.data.products.length
-    if(counter == 0){
-            itemCounterButoon.innerText='';
-            itemCounterButoon.style.display="none"
-          }
-          else{
-            itemCounterButoon.innerText=counter
-            itemCounterButoon.style.display="block"
-          }
-  
+function addProductToCart(_id, name, price, description, category, image, quantity) {
+  axios.patch("/carts/add", {
+    _id, name, price, description, category, image, quantity
   })
+    .then(response => {
+      console.log(response);
+      updateCartCounter()
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+function updateCartCounter() {
+  axios.get("/carts/619028c92121e8d9e3a2c533")
+    .then((res) => {
+      const counter = res.data.products.length
+      if (counter == 0) {
+        itemCounterButoon.innerText = '';
+        itemCounterButoon.style.display = "none"
+      }
+      else {
+        itemCounterButoon.innerText = counter
+        itemCounterButoon.style.display = "block"
+      }
+
+    })
 }
 
 updateCartCounter()
